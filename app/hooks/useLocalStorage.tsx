33083dd6588ae45403c91cace84f307182b2d9bf
Ilias Karagiannakis <liakoskari@yahoo.com>
2025-03-27 04:04:25 +0100

    M3: change avatar pictures 🖼

--- app/hooks/useLocalStorage.tsx
+++ app/hooks/useLocalStorage.tsx
@@ -6,22 +6,6 @@ interface LocalStorage<T> {
   clear: () => void;
 }
 
-/**
- * This custom function/hook safely handles SSR by checking
- * for the window before accessing browser localStorage.
- * IMPORTANT: It has a local react state AND a localStorage state.
- * When initializing the state with a default value,
- * clearing will revert to this default value for the state and
- * the corresponding token gets deleted in the localStorage.
- *
- * @param key - The key from localStorage, generic type T.
- * @param defaultValue - The default value if nothing is in localStorage yet.
- * @returns An object containing:
- *  - value: The current value (synced with localStorage).
- *  - set: Updates both react state & localStorage.
- *  - clear: Resets state to defaultValue and deletes localStorage key.
- */
-
 export default function useLocalStorage<T>(
   key: string,
   defaultValue: T
