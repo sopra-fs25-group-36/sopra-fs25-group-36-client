720c976be66b434f697c85a192d841678bbf59c7
sing-git <seungju.paek@uzh.ch>
2025-04-13 23:45:01 +0200

    fixed the merge conflicts

--- app/(auth)/lobby/[id]/game/transaction/page.tsx
+++ app/(auth)/lobby/[id]/game/transaction/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { InputNumber, Button, Typography, message } from "antd";
 import React, { useEffect, useState } from "react";
 import { useParams, useRouter } from "next/navigation";
