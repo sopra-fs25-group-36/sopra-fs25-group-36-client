f0dec67ba09b5f8f53f53f94a2376c35f6e08ae3
sing-git <seungju.paek@uzh.ch>
2025-04-24 16:23:38 +0300

    _

--- app/utils/domain.ts
+++ app/utils/domain.ts
@@ -7,7 +7,7 @@ import { isProduction } from "@/utils/environment";
  */
 export function getApiDomain(): string {
   const prodUrl = process.env.NEXT_PUBLIC_PROD_API_URL ||
-    "delta-entity-453619-p5.oa.r.appspot.com";
+    "https://delta-entity-453619-p5.oa.r.appspot.com";
   const devUrl = "http://localhost:8080";
   return isProduction() ? prodUrl : devUrl;
 }
